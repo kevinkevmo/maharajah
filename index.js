5ff78c48e7b4c9c1a21773be1fa1ec614d252fad
kevinkevmo <kevinmoriarty.nyc@gmail.com>
2020-04-25 15:38:02 -0700

    implemented diana ross mome

---
+++ index.js
@@ -0,0 +1,10 @@
+function printNextLyric(lyric) {
+    if (lyric === undefined) {
+        console.log("no lyric was entered");
+    } else if (lyric.toLowerCase() === "at first i was afraid") {
+        console.log("I was petrified");
+    }
+    
+}
+
+printNextLyric("at first i was afRaid")
